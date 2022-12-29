b84eaedfc072861cb734c22bd029f0c13e6195a0
fecheverry <fabianvargas2507@gmail.com>
2022-12-29 00:41:06 -0500

    READY TO DEPLOY PLEASE :3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Template from "@templates/Template.js";
 import "@styles/main.css";
 import "@styles/var.styl";
-console.log("Hola");
 (async function App() {
   const main = null || document.getElementById("main");
   main.innerHTML = await Template();
